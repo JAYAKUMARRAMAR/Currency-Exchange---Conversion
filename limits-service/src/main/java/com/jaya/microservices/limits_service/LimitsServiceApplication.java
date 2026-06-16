eda58ca59b637fb39a950749600284427de94abd
JAYAKUMARRAMAR <jayakumar92@gmail.com>
2026-06-16 22:40:49 +0530

    added limits file and execute main class

--- limits-service/src/main/java/com/jaya/microservices/limits_service/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/jaya/microservices/limits_service/LimitsServiceApplication.java
@@ -2,8 +2,11 @@ package com.jaya.microservices.limits_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import com.jaya.microservices.limits_service.configuration.Configuration;
 
 @SpringBootApplication
+@EnableConfigurationProperties(Configuration.class)
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
